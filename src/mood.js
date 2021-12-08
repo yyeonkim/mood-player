5cbe1b00d4c2648d84b002bd6fc42f09dfe09866
yyeonkim <seyeon.dev@gmail.com>
2021-12-08 14:10:25 +0900

    docs: Revise string to object

--- src/mood.js
+++ src/mood.js
@@ -1,7 +1,71 @@
-export const mood1 = ["설레요", "신나요", "편안해요"];
+export const mood1 = [
+  {
+    id: "1",
+    text: "설레요",
+    color: "#fd79a8",
+    selected: false,
+  },
+  {
+    id: "2",
+    text: "신나요",
+    color: "#ffeaa7",
+    selected: false,
+  },
+  {
+    id: "3",
+    text: "편안해요",
+    color: "#55efc4",
+    selected: false,
+  },
+];
 
-export const mood2 = ["답답해요", "지루해요"];
+export const mood2 = [
+  {
+    id: "4",
+    text: "답답해요",
+    color: "#F79F1F",
+    selected: false,
+  },
+  {
+    id: "5",
+    text: "지루해요",
+    color: "#b2bec3",
+    selected: false,
+  },
+];
 
-export const mood3 = ["그리워요", "속상해요", "화나요"];
+export const mood3 = [
+  {
+    id: "6",
+    text: "그리워요",
+    color: "#686de0",
+    selected: false,
+  },
+  {
+    id: "7",
+    text: "속상해요",
+    color: "#9b59b6",
+    selected: false,
+  },
+  {
+    id: "8",
+    text: "화나요",
+    color: "#e74c3c",
+    selected: false,
+  },
+];
 
-export const mood4 = ["허무해요", "야릇해요"];
+export const mood4 = [
+  {
+    id: "9",
+    text: "허무해요",
+    color: "#bdc3c7",
+    selected: false,
+  },
+  {
+    id: "10",
+    text: "야릇해요",
+    color: "#B53471",
+    selected: false,
+  },
+];

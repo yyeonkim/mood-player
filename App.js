bfc5bf1ca7398091f71cdd138cfb86be2eb5564c
yyeonkim <seyeon.dev@gmail.com>
2021-12-08 14:28:43 +0900

    chore: Add react navigation

--- App.js
+++ App.js
@@ -1,41 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Mood from "./src/components/Mood";
+import Home from "./src/pages/Home";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text style={[styles.title, styles.text]}>오늘 기분이 어떠신가요?</Text>
-      <Text style={[styles.text, styles.subTitle]}>
-        아래 키워드를 선택해주세요 (2개)
-      </Text>
-      <Mood />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#21232B",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  text: {
-    color: "white",
-  },
-
-  title: {
-    fontSize: 25,
-  },
-
-  subTitle: {
-    fontSize: 18,
-    marginTop: 10,
-    color: "rgba(256, 256, 256, .8)",
-  },
-});
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,41 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import Mood from "../components/Mood";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text style={[styles.title, styles.text]}>오늘 기분이 어떠신가요?</Text>
+      <Text style={[styles.text, styles.subTitle]}>
+        아래 키워드를 선택해주세요 (2개)
+      </Text>
+      <Mood />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#21232B",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  text: {
+    color: "white",
+  },
+
+  title: {
+    fontSize: 25,
+  },
+
+  subTitle: {
+    fontSize: 18,
+    marginTop: 10,
+    color: "rgba(256, 256, 256, .8)",
+  },
+});

a3c72c09b96a4f64a699c0b044f762f431d19191
yyeonkim <seyeon.dev@gmail.com>
2021-12-14 00:55:21 +0900

    feat: Add spotify iframe by using WebView

---
+++ src/components/Loading.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.title, styles.text]}>
+        이 노래를 추천드리고 싶네요
+      </Text>
+      <Text style={[styles.text, styles.subTitle]}>
+        노래를 가져오는 중입니다.
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#21232B",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  text: {
+    color: "white",
+  },
+
+  title: {
+    fontSize: 25,
+  },
+
+  subTitle: {
+    fontSize: 18,
+    marginTop: 10,
+    color: "rgba(256, 256, 256, .8)",
+  },
+});
--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,23 +1,45 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import WebView from "react-native-webview";
 
-export default function Music({ navigation }) {
+const windowWidth = Dimensions.get("window").width;
+
+export default function Music() {
   return (
-    <View style={styles.container}>
-      <Text style={[styles.title, styles.text]}>
-        이 노래를 추천드리고 싶네요
-      </Text>
-      <Text style={[styles.text, styles.subTitle]}>
-        노래를 가져오는 중입니다.
-      </Text>
-    </View>
+    <WebView
+      scalesPageToFit={true}
+      bounces={false}
+      javaScriptEnabled
+      style={styles.playlist}
+      source={{
+        html: `
+                  <!DOCTYPE html>
+                  <html>
+                    <head></head> 
+                    <body>
+                      <div id="baseDiv">
+                        <iframe
+                        src="https://open.spotify.com/embed/playlist/7ghvOyDA3MuhHgIdcKeGRf?utm_source=generator"
+                        width="100%"
+                        height="80"
+                        frameBorder="0"
+                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                      ></iframe>
+                    </div>
+                    </body>
+                  </html>
+            `,
+      }}
+      automaticallyAdjustContentInsets={false}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  playlist: {
     backgroundColor: "#21232B",
+    flex: 1,
+    flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
   },

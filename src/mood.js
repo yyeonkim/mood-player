517b9ee683ef76f0a772b5d57785bdbfa6a75723
yyeonkim <seyeon.dev@gmail.com>
2021-12-14 02:50:51 +0900

    docs: Add keyword to mood object

--- src/mood.js
+++ src/mood.js
@@ -4,18 +4,21 @@ export const mood1 = [
     text: "설레요",
     color: "#fd79a8",
     selected: false,
+    keyword: "love",
   },
   {
     id: "2",
     text: "신나요",
     color: "#FFC312",
     selected: false,
+    keyword: "excited",
   },
   {
     id: "3",
     text: "편안해요",
     color: "#55efc4",
     selected: false,
+    keyword: "calm",
   },
 ];
 
@@ -25,12 +28,14 @@ export const mood2 = [
     text: "답답해요",
     color: "#F79F1F",
     selected: false,
+    keyword: "frustrated",
   },
   {
     id: "5",
     text: "지루해요",
     color: "#133936",
     selected: false,
+    kerword: "bored",
   },
 ];
 
@@ -40,18 +45,21 @@ export const mood3 = [
     text: "그리워요",
     color: "#686de0",
     selected: false,
+    kerword: "lonely",
   },
   {
     id: "7",
     text: "속상해요",
     color: "#9b59b6",
     selected: false,
+    kerword: "sad",
   },
   {
     id: "8",
     text: "화나요",
     color: "#e74c3c",
     selected: false,
+    kerword: "angry",
   },
 ];
 
@@ -61,11 +69,13 @@ export const mood4 = [
     text: "허무해요",
     color: "#4b6584",
     selected: false,
+    kerword: "empty",
   },
   {
     id: "10",
     text: "야릇해요",
     color: "#B53471",
     selected: false,
+    kerword: "chic",
   },
 ];

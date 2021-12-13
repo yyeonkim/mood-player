7137c0ab772b24680d416e101d70b6a4eddc49c7
yyeonkim <seyeon.dev@gmail.com>
2021-12-14 01:49:14 +0900

    Add Playlist component to Music screen

---
+++ src/components/Playlist.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import WebView from "react-native-webview";
+
+const playlistId = "7ghvOyDA3MuhHgIdcKeGRf";
+
+export default function Playlist() {
+  return (
+    <View style={styles.playlist}>
+      <WebView
+        scalesPageToFit={true}
+        bounces={false}
+        javaScriptEnabled
+        source={{
+          html: `
+                    <!DOCTYPE html>
+                    <html>
+                      <head>
+                      <style>
+                        body {
+                            background-color: #21232B;
+                        }
+                        
+                        .player {
+                            position: absolute;
+                            width: 100%;
+                            left: 0;
+                        }
+                      </style>
+                      </head> 
+                      <body>
+                          <iframe class="player" src="https://open.spotify.com/embed/playlist/${playlistId}?utm_source=generator&theme=0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+                      </body>
+                    </html>
+              `,
+        }}
+        automaticallyAdjustContentInsets={false}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  playlist: {
+    height: 27,
+  },
+});
--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,47 +1,21 @@
 import React from "react";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
-import WebView from "react-native-webview";
+import { StyleSheet, Text, View, Dimensions, StatusBar } from "react-native";
 
-const windowWidth = Dimensions.get("window").width;
+import Playlist from "../components/Playlist";
 
 export default function Music() {
   return (
-    <WebView
-      scalesPageToFit={true}
-      bounces={false}
-      javaScriptEnabled
-      style={styles.playlist}
-      source={{
-        html: `
-                  <!DOCTYPE html>
-                  <html>
-                    <head></head> 
-                    <body>
-                      <div id="baseDiv">
-                        <iframe
-                        src="https://open.spotify.com/embed/playlist/7ghvOyDA3MuhHgIdcKeGRf?utm_source=generator"
-                        width="100%"
-                        height="80"
-                        frameBorder="0"
-                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                      ></iframe>
-                    </div>
-                    </body>
-                  </html>
-            `,
-      }}
-      automaticallyAdjustContentInsets={false}
-    />
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <Playlist />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  playlist: {
+  container: {
     backgroundColor: "#21232B",
     flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
   },
 
   text: {
